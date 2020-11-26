fd779cf62bed4f1023eef2a778e4ae2ee1852c4c
MestreALMO <lusegardis@gmail.com>
2020-11-25 21:18:50 -0300

    1 task left, bug fix

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,44 +1,44 @@
-import csvParse from 'csv-parse';
-import fs from 'fs';
-import Transaction from '../models/Transaction';
+// import csvParse from 'csv-parse';
+// import fs from 'fs';
+// import Transaction from '../models/Transaction';
 
-interface CSVTransaction {
-  title: string;
-  type: 'income' | 'outcome';
-  value: number;
-  category: string;
-}
+// interface CSVTransaction {
+//   title: string;
+//   type: 'income' | 'outcome';
+//   value: number;
+//   category: string;
+// }
 
-class ImportTransactionsService {
-  async execute(filePath: string): Promise<Transaction[]> {
-    const contactsReadStream = fs.createReadStream('filePath');
+// class ImportTransactionsService {
+//   async execute(filePath: string): Promise<Transaction[]> {
+//     const contactsReadStream = fs.createReadStream('filePath');
 
-    const parsers = csvParse({
-      from_line: 2,
-    });
+//     const parsers = csvParse({
+//       from_line: 2,
+//     });
 
-    const parseCSV = contactsReadStream.pipe(parsers);
+//     const parseCSV = contactsReadStream.pipe(parsers);
 
-    const transactions: CSVTransaction[] = [];
-    const categories: string[] = [];
+//     const transactions: CSVTransaction[] = [];
+//     const categories: string[] = [];
 
-    parseCSV.on('data', async line => {
-      const [title, type, value, category] = line.map((cell: string) =>
-        cell.trim(),
-      );
+//     parseCSV.on('data', async line => {
+//       const [title, type, value, category] = line.map((cell: string) =>
+//         cell.trim(),
+//       );
 
-      if (!title || !type || !value) return;
+//       if (!title || !type || !value) return;
 
-      categories.push(category);
+//       categories.push(category);
 
-      transactions.push({ title, type, value, category });
-    });
+//       transactions.push({ title, type, value, category });
+//     });
 
-    await new Promise(resolve => parseCSV.on('end', resolve));
+//     await new Promise(resolve => parseCSV.on('end', resolve));
 
-    console.log(categories);
-    console.log(transactions);
-  }
-}
+//     console.log(categories);
+//     console.log(transactions);
+//   }
+// }
 
-export default ImportTransactionsService;
+// export default ImportTransactionsService;
